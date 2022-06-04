059d8d8ea82c6af50e95204a1f2cac392a35abc5
luisfontalv <lfontalv30@gmail.com>
2022-06-04 00:34:31 -0500

    add cdn boostrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
 import './assets/css/index.css';
 import FutballApp from './components/FutballApp';
